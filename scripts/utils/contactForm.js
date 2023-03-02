b8e4a323cb4938df46bb57755724a47cb7a02b9b
nosfair <nosfairalchtion@gmail.com>
2023-03-02 17:53:47 +0100

    complete js + rename folder + icons

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,77 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+//check "prénom" data
+function FirstName() {
+    let inputValue = document.getElementById("prenom").value;
+    if (inputValue !== "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//check "nom de famille" data
+function LastName() {
+    let inputValue = document.getElementById("nom").value;
+    if (inputValue !== "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//check "email" data
+function Email() {
+    let inputValue = document.getElementById("email").value;
+    if (inputValue !== "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function message() {
+    let inputValue = document.getElementById("message").value;
+    if (inputValue !== "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// fonction pour valider le formulaire
+document.querySelector("#submit").addEventListener("click", (e) => {
+    // "annuler" le comportement par défaut
+    e.preventDefault();
+
+
+    // vérifier un par un tous les champs
+    const verifFirstName = FirstName();
+    const verifLastName = LastName();
+    const verifEmail = Email();
+    const verifMessage = message();
+
+
+    // si tout est vrai 
+    if (verifFirstName && verifLastName && verifEmail && verifMessage) {
+        console.log(document.getElementById("prenom").value);
+        console.log(document.getElementById("nom").value);
+        console.log(document.getElementById("email").value);
+        console.log(document.getElementById("message").value);
+    }
+    else {
+        console.log("Il y a une erreur");
+    }
+})
---
+++ scripts/utils/select.js
@@ -0,0 +1,208 @@
+window.onload = () => {
+    const selectElt = document.querySelector("select");
+    const selectDiv = document.querySelector(".custom-select");
+    const newSelect = document.createElement("div");
+    newSelect.classList.add("new-select");
+    newSelect.innerHTML = selectElt.options[selectElt.selectedIndex].innerHTML;
+    selectDiv.appendChild(newSelect);
+
+    const newMenu = document.createElement("div");
+    newMenu.classList.add("select-items", "select-hide");
+    for (let option of selectElt.options) {
+        const newOption = document.createElement("div");
+        newOption.innerHTML = option.innerHTML;
+        newOption.id = option.id + "-new";
+        newOption.addEventListener("click", function () {
+            for (let option of selectElt.options) {
+                if (option.innerHTML === this.innerHTML) {
+                    selectElt.selectedIndex = option.index;
+                    newSelect.innerHTML = this.innerHTML;
+                    break;
+                }
+            }
+            newSelect.click();
+        })
+        newMenu.appendChild(newOption);
+    }
+    selectDiv.appendChild(newMenu);
+
+    newSelect.addEventListener("click", function (e) {
+        e.stopPropagation();
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("active");
+    })
+
+    const like = document.querySelector("#likes-new")
+    async function likeSort() {
+        const photographers = await getPhotographers();
+        const profil = photographers.find((photo) =>
+            photo.id == idUrl
+        );
+        const mediaSource = await getMedia();
+        let medias = mediaSource.filter((m) =>
+            m.photographerId == idUrl
+        )
+        medias = medias.sort(function (a, b) {
+            if (a.likes < b.likes) {
+                return 1;
+            }
+            if (a.likes > b.likes) {
+                return -1;
+            }
+            return 0;
+        });
+        console.table(medias)
+        document.querySelector(".pictures_medias").innerHTML = "";
+        document.querySelector(".lightbox_container").innerHTML = "";
+        medias.forEach((medias) => {
+            const photoCard = mediasFactory(medias, profil.name);
+            const MediasCardDOM = photoCard.getMediasDOM();
+            const MediasCardLightbox = photoCard.getMediasLightbox();
+            document.querySelector(".pictures_medias").appendChild(MediasCardDOM);
+            document.querySelector(".lightbox_container").appendChild(MediasCardLightbox);
+        });
+        let clickLightbox = document.querySelectorAll(".photos, .videos")
+        console.table(clickLightbox)
+        for (let i = 0; i < clickLightbox.length; i++) {
+            clickLightbox[i].addEventListener("click", () => {
+                displayLightbox(i)
+            })
+        }
+        const lightbox = document.querySelector(".lightbox")
+        slides = Array.from(document.querySelector(".lightbox_container").children)
+
+        slideWidth = document.querySelector(".lightbox_container").getBoundingClientRect().width
+
+        let next = document.querySelector(".arrow_right")
+        let prev = document.querySelector(".arrow_left")
+
+        next.addEventListener("click", slideNext)
+        prev.addEventListener("click", slidePrev)
+
+        let decal = -slideWidth * compteur
+
+        let photos = document.querySelectorAll(".media_lightbox")
+
+        photos.forEach(element => {
+            element.style.transform = `translateX(${decal}px)`;
+        });
+    }
+    like.addEventListener("click", likeSort)
+
+    //tri par titre
+    const titles = document.querySelector("#titre-new")
+    async function titleSort() {
+        const photographers = await getPhotographers();
+        const profil = photographers.find((photo) =>
+            photo.id == idUrl
+        );
+        const mediaSource = await getMedia();
+        let medias = mediaSource.filter((m) =>
+            m.photographerId == idUrl
+        )
+        medias = medias.sort(function (a, b) {
+            if (a.title < b.title) {
+                return -1;
+            }
+            if (a.title > b.title) {
+                return 1;
+            }
+            return 0;
+        });
+        console.table(medias)
+        document.querySelector(".pictures_medias").innerHTML = "";
+        document.querySelector(".lightbox_container").innerHTML = "";
+        medias.forEach((medias) => {
+            const photoCard = mediasFactory(medias, profil.name);
+            const MediasCardDOM = photoCard.getMediasDOM();
+            const MediasCardLightbox = photoCard.getMediasLightbox();
+            document.querySelector(".pictures_medias").appendChild(MediasCardDOM);
+            document.querySelector(".lightbox_container").appendChild(MediasCardLightbox);
+        });
+        let clickLightbox = document.querySelectorAll(".photos, .videos")
+        console.table(clickLightbox)
+        for (let i = 0; i < clickLightbox.length; i++) {
+            clickLightbox[i].addEventListener("click", () => {
+                displayLightbox(i)
+            })
+        }
+        const lightbox = document.querySelector(".lightbox")
+        slides = Array.from(document.querySelector(".lightbox_container").children)
+
+        slideWidth = document.querySelector(".lightbox_container").getBoundingClientRect().width
+
+        let next = document.querySelector(".arrow_right")
+        let prev = document.querySelector(".arrow_left")
+
+        next.addEventListener("click", slideNext)
+        prev.addEventListener("click", slidePrev)
+
+        let decal = -slideWidth * compteur
+
+        let photos = document.querySelectorAll(".media_lightbox")
+
+        photos.forEach(element => {
+            element.style.transform = `translateX(${decal}px)`;
+        });
+    }
+    titles.addEventListener("click", titleSort)
+
+    //tri par date
+    const mediaDate = document.querySelector("#date-new")
+    async function dateSort() {
+        const photographers = await getPhotographers();
+        const profil = photographers.find((photo) =>
+            photo.id == idUrl
+        );
+        const mediaSource = await getMedia();
+        let medias = mediaSource.filter((m) =>
+            m.photographerId == idUrl
+        )
+        medias = medias.sort(function (a, b) {
+            if (a.date < b.date) {
+                return 1;
+            }
+            if (a.date > b.date) {
+                return -1;
+            }
+            return 0;
+        });
+        console.table(medias)
+        document.querySelector(".pictures_medias").innerHTML = "";
+        document.querySelector(".lightbox_container").innerHTML = "";
+        medias.forEach((medias) => {
+            const photoCard = mediasFactory(medias, profil.name);
+            const MediasCardDOM = photoCard.getMediasDOM();
+            const MediasCardLightbox = photoCard.getMediasLightbox();
+            document.querySelector(".pictures_medias").appendChild(MediasCardDOM);
+            document.querySelector(".lightbox_container").appendChild(MediasCardLightbox);
+        });
+        let clickLightbox = document.querySelectorAll(".photos, .videos")
+        console.table(clickLightbox)
+        for (let i = 0; i < clickLightbox.length; i++) {
+            clickLightbox[i].addEventListener("click", () => {
+                displayLightbox(i)
+            })
+        }
+        const lightbox = document.querySelector(".lightbox")
+        slides = Array.from(document.querySelector(".lightbox_container").children)
+
+        slideWidth = document.querySelector(".lightbox_container").getBoundingClientRect().width
+
+        let next = document.querySelector(".arrow_right")
+        let prev = document.querySelector(".arrow_left")
+
+        next.addEventListener("click", slideNext)
+        prev.addEventListener("click", slidePrev)
+
+        let decal = -slideWidth * compteur
+
+        let photos = document.querySelectorAll(".media_lightbox")
+
+        photos.forEach(element => {
+            element.style.transform = `translateX(${decal}px)`;
+        });
+    }
+    mediaDate.addEventListener("click", dateSort)
+
+}
