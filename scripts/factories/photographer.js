eb67c85beb2bfd6288781a7fc86c5e6a72bd7386
nosfair <nosfairalchtion@gmail.com>
2023-03-02 13:48:17 +0100

    add css for index + adjust js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,44 @@
+// generates photographers cards in home page
 function photographerFactory(data) {
-    const { name, portrait, city, country, id, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const url = `photographer.html?id=${id}`;
 
+    // generates user card DOM element
     function getUserCardDOM() {
+        const redirect = document.createElement('a');
+        redirect.setAttribute('href', url)
         const article = document.createElement('article');
-        const link = document.createElement('a');
-        link.setAttribute("href", "photographer.html?id=" + id)
+        const container_img = document.createElement('div');
         const img = document.createElement('img');
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "Image_profil")
         const h2 = document.createElement('h2');
+
+        const location = document.createElement('span');
+        const sentence = document.createElement('span');
+        const dayprice = document.createElement('span');
+
+
+        container_img.classList.add('container_img');
+        location.classList.add('location');
+        sentence.classList.add('tagline');
+        dayprice.classList.add('dayprice');
+
+        location.textContent = city + ', ' + country;
+        sentence.textContent = tagline;
+        dayprice.textContent = price + '€/jour';
         h2.textContent = name;
-        const h3 = document.createElement('h3');
-        h3.textContent = `${city}, ${country}`;
-        const slogan = document.createElement('p');
-        slogan.textContent = tagline;
-        slogan.classList.add("slogan");
-        const cost = document.createElement('p');
-        cost.textContent = price + " €/jour";
-        cost.classList.add("prix");
-        link.appendChild(img);
-        link.appendChild(h2);
-        article.appendChild(link);
-        article.appendChild(h3);
-        article.appendChild(slogan);
-        article.appendChild(cost);
-        return (article);
+
+        redirect.appendChild(article);
+        article.appendChild(container_img);
+        container_img.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(sentence);
+        article.appendChild(dayprice);
+        return (redirect);
     }
     return { name, picture, getUserCardDOM }
-
 }
