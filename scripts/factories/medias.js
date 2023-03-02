2196fb4ca5348d3c3a5841917ba0d3fbe317f999
nosfair <nosfairalchtion@gmail.com>
2023-03-02 15:55:52 +0100

    add js, css and html for photographer

---
+++ scripts/factories/medias.js
@@ -0,0 +1,87 @@
+function mediasFactory(data, name) {
+    const { image, video, title, likes } = data;
+
+    const coeur = `assets/icons/heart.svg`;
+
+    let medias;
+    if (image != undefined) {
+        medias = `assets/images/${name.split(" ")[0]}/${image}`;
+    }
+    else {
+        medias = `assets/images/${name.split(" ")[0]}/${video}`;
+    }
+
+    function getMediasDOM() {
+        const article = document.createElement('article');
+        if (image != undefined) {
+            const image = document.createElement('img');
+            image.setAttribute("src", medias);
+            image.className = 'photos';
+            article.appendChild(image);
+        }
+        else {
+            const video = document.createElement('video');
+            const source = document.createElement('source');
+            source.setAttribute("src", medias);
+            video.className = 'videos';
+            video.appendChild(source);
+            article.appendChild(video);
+        }
+        const legende = document.createElement('div');
+        const titre = document.createElement('span');
+        titre.classList.add('title');
+        titre.textContent = title;
+        const nbLikes = document.createElement('span');
+        nbLikes.classList.add('likes');
+        nbLikes.textContent = likes;
+        const img = document.createElement('img');
+        img.setAttribute("src", coeur);
+        img.className = 'coeur';
+        img.addEventListener("click", addLike);
+        img.addEventListener("click", photoLike);
+        legende.classList.add("media_infos");
+        legende.appendChild(titre);
+        legende.appendChild(nbLikes);
+        legende.appendChild(img);
+        article.appendChild(legende);
+        return (article);
+    }
+
+    function getMediasLightbox() {
+        const media = document.createElement('div');
+        media.className = 'media_lightbox';
+        if (image != undefined) {
+            const image = document.createElement('img');
+            image.setAttribute("src", medias);
+            image.className = 'photos_lightbox';
+            media.appendChild(image);
+        }
+        else {
+            const video = document.createElement('video');
+            const source = document.createElement('source');
+            source.setAttribute("src", medias);
+            video.className = 'photos_lightbox';
+            video.appendChild(source);
+            media.appendChild(video);
+        }
+        const h2 = document.createElement('div');
+        h2.textContent = `${title}`;
+        h2.classList.add("media_infos");
+        media.appendChild(h2);
+        return (media);
+    }
+
+    return { getMediasDOM, getMediasLightbox, }
+}
+
+function addLike() {
+    let like = document.querySelector(".totalLikes").textContent;
+    like = parseInt(like) + 1;
+    document.querySelector(".totalLikes").textContent = like;
+}
+
+function photoLike(e) {
+    let addHeart = e.target.previousSibling.textContent;
+    addHeart = parseInt(addHeart) + 1;
+    e.target.previousSibling.textContent = addHeart;
+}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,94 @@
+/*global mediasFactory, displayLightbox*/
+
 //Mettre le code JavaScript lié à la page photographer.html
+let params = (new URL(document.location)).searchParams;
+let idUrl = params.get('id');
+
+async function getPhotographers() {
+    const response = await fetch('data/photographers.json')
+    const fichierjson = await response.json();
+    return fichierjson.photographers;
+}
+async function getMedia() {
+    const response = await fetch('data/photographers.json')
+    const fichierjson = await response.json();
+    return fichierjson.media;
+}
+
+async function init() {
+    const photographers = await getPhotographers();
+    // je récupère le photographe lié à l'id de l'URL
+    const profil = photographers.find((photo) =>
+        photo.id == idUrl
+    );
+
+    // si pas (ou mauvais) id je reviens sur la page index
+    if (profil == undefined) {
+        window.location.href = "/"
+    }
+    const photographerInfo = document.querySelector(".photographer_profile");
+    const photographeHeader = document.createElement("article");
+    const picture = `assets/photographers/${profil.portrait}`;
+    const img = document.createElement('img');
+    img.setAttribute("src", picture);
+    img.classList.add("profilePicture");
+    let info = `<h1 class ="photographer_name">${profil.name}</h1>
+            <p class="photographer_location">${profil.city}, ${profil.country}</p>
+            <p class="photographer_devise">${profil.tagline}</p>`;
+    photographerInfo.prepend(photographeHeader);
+    photographeHeader.innerHTML = info;
+    photographerInfo.appendChild(img);
+
+    const mediaSource = await getMedia();
+    // je récupère les médias liés à l'id de l'URL
+    let medias = mediaSource.filter((m) =>
+        m.photographerId == idUrl
+    )
+
+    // je trie les médias par ordre alphabétique
+    medias = medias.sort(function (a, b) {
+        if (a.title < b.title) {
+            return -1;
+        }
+        if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    });
+
+    //j'affiche les médias
+    medias.forEach((medias) => {
+        const photoCard = mediasFactory(medias, profil.name);
+        const MediasCardDOM = photoCard.getMediasDOM();
+        const MediasCardLightbox = photoCard.getMediasLightbox();
+        document.querySelector(".pictures_medias").appendChild(MediasCardDOM);
+        document.querySelector(".lightbox_container").appendChild(MediasCardLightbox);
+    });
+
+    //gestion de l'ouverture de la lightbox
+    let clickLightbox = document.querySelectorAll(".photos, .videos")
+    for (let i = 0; i < clickLightbox.length; i++) {
+        clickLightbox[i].addEventListener("click", () => {
+            displayLightbox(i)
+        })
+    }
+
+
+    //calcul des likes
+    let totalLikes = 0
+    medias.forEach((media) => {
+        totalLikes += media.likes
+    });
+
+    //j'affiche les élements dans la likebox
+    const photographerLikesBox = document.querySelector(".box");
+    const photographerLikes = document.createElement("div");
+    let infoLikes = `<div class="boxInfos"> 
+  <div class=""><span class="totalLikes">${totalLikes}</span> <img src="assets/icons/heart-solid.svg" width="18px" height="18px"></div>
+  <div class="boxPrice">${profil.price}€ / jour</div></div>`;
+    photographerLikesBox.prepend(photographerLikes);
+    photographerLikes.innerHTML = infoLikes;
+
+}
+
+init();
